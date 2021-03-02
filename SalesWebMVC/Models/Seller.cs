d923eedb547fd93ab0800ed60abc3b614fff2c39
juliana-santiago <juju_msantiago@hotmail.com>
2021-03-02 20:56:26 -0300

    Other entities and second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalayr { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalayr, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalayr = baseSalayr;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
