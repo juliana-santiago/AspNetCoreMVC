8fd1b9b22dd84b3b75c68d374f7b383ebe1b5bb9
juliana-santiago <juju_msantiago@hotmail.com>
2021-03-04 17:09:15 -0300

    Implementing grouping search

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -8,10 +8,10 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
 
-        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
