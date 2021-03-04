de2f76ee836803b4c5474bb05ec4493f9689c1be
juliana-santiago <juju_msantiago@hotmail.com>
2021-03-04 17:01:03 -0300

    Preparing sales search navigation views and Implementing simple search

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -47,6 +47,7 @@ builder.MigrationsAssembly("SalesWebMVC")));
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
